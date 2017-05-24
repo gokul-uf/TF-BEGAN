a2071fdc591d3992215ff313504f07a064f77a57
gokul <gokul.uf@gmail.com>
2017-05-25 00:03:19 +0200

    added the configuration params in config.py

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 import tensorflow as tf
 
 class Config:
-	batch_size = 10
-	n = 5
-	embedding_dim = 5
+	batch_size = 32
+	n = 13
+	embedding_dim = 32
+	img_width = 64
+	img_height = 64
+	num_channel = 3
+	lambda_k = 0.001
