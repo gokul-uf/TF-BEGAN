c29389760e80e2e901e6a45caa2d9b58854577ad
gokul <gokul.uf@gmail.com>
2017-05-25 18:41:54 +0200

    added params for sampling in config

--- config.py
+++ config.py
@@ -1,10 +1,26 @@
 import tensorflow as tf
 
 class Config:
-	batch_size = 32
-	n = 13
-	embedding_dim = 32
+	batch_size = 16
+	n = 13 # TODO: change
+
+	embedding_dim = 64
+	z_low_limit = -1
+	z_high_limit = 1
+
 	img_width = 64
 	img_height = 64
 	num_channel = 3
+
 	lambda_k = 0.001
+	gamma = 0.5
+	k_0 = 0
+	lr = 5e-5
+
+	sample_location = "../samples"
+	num_samples = 20
+
+	num_epoch = 1000
+	sample_epoch = 5 # generate samples every 5 epochs
+	dataset_size = 202599 #CelebA dataset size
+	data_location = "../data/img_align_celeba/"
